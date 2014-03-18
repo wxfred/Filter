360ac128000eaa169834fb8c6116bb3a91ec4b6e
wxfred <438790334@qq.com>
2014-03-19 00:24:15 +0800

    Window scaling

--- Filter Zero/Filter Zero/CScaleEventFilter.cpp
+++ Filter Zero/Filter Zero/CScaleEventFilter.cpp
@@ -2,12 +2,31 @@
 #include "CMainWindow.h"
 #include <QMainWindow>
 #include <QMouseEvent>
+#include <QRect>
 
-#define SIZE_LEFT 10
-#define SIZE_RIGHT 10
-#define SIZE_TOP 10
-#define SIZE_BOTTOM 10
+#define REGION_LEFT 10
+#define REGION_RIGHT 10
+#define REGION_TOP 10
+#define REGION_BOTTOM 10
 
+//////////////////////////////////////////////////////////////////////////
+// Public method(s)
+//////////////////////////////////////////////////////////////////////////
+
+/** Construct method */
+CScaleEventFilter::CScaleEventFilter(QMainWindow *parent)
+	:QObject(parent)
+{
+	m_pMainWindow = NULL;
+	m_bLeftButtonDown = false;
+	m_eWindowRegionUnderMouse = CENTER;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Protected method(s)
+//////////////////////////////////////////////////////////////////////////
+
+/** Override method */
 bool CScaleEventFilter::eventFilter(QObject *obj, QEvent *event)
 {
 	m_pMainWindow = (QMainWindow *)obj;
@@ -28,47 +47,131 @@ bool CScaleEventFilter::eventFilter(QObject *obj, QEvent *event)
 	return false;
 }
 
+/**
+ * 
+ */
 void CScaleEventFilter::mousePressEvent(QMouseEvent *event)
 {
 	if (event->button() == Qt::LeftButton)
 	{
 		m_posInWindow = event->globalPos() - m_pMainWindow->frameGeometry().topLeft();
+		m_eWindowRegionUnderMouse = windowRegion(m_posInWindow);
 		m_bLeftButtonDown = true;
 	}
 }
 
+/**
+ * 
+ */
 void CScaleEventFilter::mouseMoveEvent(QMouseEvent *event)
 {
 	QPoint pos = m_pMainWindow->mapTo(m_pMainWindow, event->pos());
 
-	if (m_bLeftButtonDown == true)
+	if (m_bLeftButtonDown == false)
 	{
-		m_pMainWindow->move(event->globalPos() - m_posInWindow);
+		EWindowRegion region = windowRegion(pos);
+		setCursor(region);
 	}
-
-	m_pMainWindow->setCursor(Qt::ArrowCursor);
-	m_bOnLeft = false;
-	m_bOnRight = false;
-	m_bOnTop = false;
-	m_bOnBottom = false;
-	if (pos.x() < SIZE_LEFT) m_bOnLeft = true;
-	if (pos.x() > m_pMainWindow->width()-SIZE_RIGHT) m_bOnRight = true;
-	if (pos.y() < SIZE_TOP) m_bOnTop = true;
-	if (pos.y() > m_pMainWindow->height()-SIZE_BOTTOM) m_bOnBottom = true;
-	if (m_bOnLeft && m_bOnTop) m_pMainWindow->setCursor(Qt::SizeFDiagCursor);
-	else if (m_bOnLeft && m_bOnBottom) m_pMainWindow->setCursor(Qt::SizeBDiagCursor);
-	else if (m_bOnRight && m_bOnTop) m_pMainWindow->setCursor(Qt::SizeBDiagCursor);
-	else if (m_bOnRight && m_bOnBottom) m_pMainWindow->setCursor(Qt::SizeFDiagCursor);
-	else if (m_bOnLeft) m_pMainWindow->setCursor(Qt::SizeHorCursor);
-	else if (m_bOnRight) m_pMainWindow->setCursor(Qt::SizeHorCursor);
-	else if (m_bOnTop) m_pMainWindow->setCursor(Qt::SizeVerCursor);
-	else if (m_bOnBottom) m_pMainWindow->setCursor(Qt::SizeVerCursor);
+	else
+	{
+		QRect geometry = m_pMainWindow->geometry();
+		QPoint globalPos = event->globalPos();
+		switch (m_eWindowRegionUnderMouse)
+		{
+		case TOP_LEFT:
+			geometry.setTopLeft(QPoint(globalPos.x(), globalPos.y()));
+			break;
+		case TOP:
+			geometry.setTop(globalPos.y());
+			break;
+		case TOP_RIGHT:
+			geometry.setTopRight(QPoint(globalPos.x(),globalPos.y()));
+			break;
+		case RIGHT:
+			geometry.setRight(globalPos.x());
+			break;
+		case BOTTOM_RIGHT:
+			geometry.setBottomRight(QPoint(globalPos.x(), globalPos.y()));
+			break;
+		case BOTTOM:
+			geometry.setBottom(globalPos.y());
+			break;
+		case BOTTOM_LEFT:
+			geometry.setBottomLeft(QPoint(globalPos.x(), globalPos.y()));
+			break;
+		case LEFT:
+			geometry.setLeft(globalPos.x());
+			break;
+		case CENTER:
+			geometry.moveTo(globalPos-m_posInWindow);
+			break;
+		}
+		m_pMainWindow->setGeometry(geometry);
+	}
 }
 
+/**
+ * 
+ */
 void CScaleEventFilter::mouseReleaseEvent(QMouseEvent *event)
 {
 	if (event->button() == Qt::LeftButton)
 	{
 		m_bLeftButtonDown = false;
+	}
+}
+
+/**
+ * 
+ */
+EWindowRegion CScaleEventFilter::windowRegion(QPoint pos)
+{
+	bool onLeft = false;
+	bool onRight = false;
+	bool onTop = false;
+	bool onBottom = false;
+
+	if (pos.x() < REGION_LEFT) onLeft = true;
+	if (pos.x() > m_pMainWindow->width()-REGION_RIGHT) onRight = true;
+	if (pos.y() < REGION_TOP) onTop = true;
+	if (pos.y() > m_pMainWindow->height()-REGION_BOTTOM) onBottom = true;
+
+	if (onLeft && onTop) return TOP_LEFT;
+	if (onLeft && onBottom) return BOTTOM_LEFT;
+	if (onRight && onTop) return TOP_RIGHT;
+	if (onRight && onBottom) return BOTTOM_RIGHT;
+	if (onLeft) return LEFT;
+	if (onRight) return RIGHT;
+	if (onTop) return TOP;
+	if (onBottom) return BOTTOM;
+	return CENTER;
+}
+
+/**
+ * 
+ */
+void CScaleEventFilter::setCursor(EWindowRegion region)
+{
+	switch (region)
+	{
+	case TOP_LEFT:
+	case BOTTOM_RIGHT:
+		m_pMainWindow->setCursor(Qt::SizeFDiagCursor);
+		break;
+	case TOP:
+	case BOTTOM:
+		m_pMainWindow->setCursor(Qt::SizeVerCursor);
+		break;
+	case TOP_RIGHT:
+	case BOTTOM_LEFT:
+		m_pMainWindow->setCursor(Qt::SizeBDiagCursor);
+		break;
+	case RIGHT:
+	case LEFT:
+		m_pMainWindow->setCursor(Qt::SizeHorCursor);
+		break;
+	case CENTER:
+		m_pMainWindow->setCursor(Qt::ArrowCursor);
+		break;
 	}
 }
--- Filter Zero/Filter Zero/CScaleEventFilter.h
+++ Filter Zero/Filter Zero/CScaleEventFilter.h
@@ -6,24 +6,38 @@
 #include <QMouseEvent>
 #include <QPoint>
 
+enum EWindowRegion
+{
+	TOP_LEFT,
+	TOP,
+	TOP_RIGHT,
+	RIGHT,
+	BOTTOM_RIGHT,
+	BOTTOM,
+	BOTTOM_LEFT,
+	LEFT,
+	CENTER
+};
+
 class CScaleEventFilter : public QObject
 {
 	Q_OBJECT
+
+public:
+	CScaleEventFilter(QMainWindow *parent = 0);
 
 protected:
 	bool eventFilter(QObject *obj, QEvent *event);
 	void mousePressEvent(QMouseEvent *event);
 	void mouseMoveEvent(QMouseEvent *event);
 	void mouseReleaseEvent(QMouseEvent *event);
+	EWindowRegion windowRegion(QPoint pos);
+	void setCursor(EWindowRegion region);
 private:
-	QMainWindow* m_pMainWindow;
-	QPoint m_posInWindow;
-	bool m_bLeftButtonDown;
-
-	bool m_bOnLeft;
-	bool m_bOnRight;
-	bool m_bOnTop;
-	bool m_bOnBottom;
+	QMainWindow*	m_pMainWindow;
+	QPoint			m_posInWindow;
+	bool			m_bLeftButtonDown;
+	EWindowRegion	m_eWindowRegionUnderMouse;
 };
 
 #endif
